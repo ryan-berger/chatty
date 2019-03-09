3a747eae88df58c9f51e1d4538f5797b90fff5e6
Ryan Berger <ryanbberger@gmail.com>
2019-03-09 16:50:58 -0700

    fix struct tags

--- repositories/model.go
+++ repositories/model.go
@@ -11,17 +11,17 @@ type Conversant struct {
 // Message is an incoming message to be sent to all conversants
 // within the given conversation
 type Message struct {
-	ID             string
-	SenderID       string
-	Message        string
-	ConversationID string
+	ID             string `db:"id"`
+	SenderID       string `db:"sender_id"`
+	Message        string `db:"message"`
+	ConversationID string `db:"conversation_id"`
 }
 
 // Conversation is a group of conversants, and a list of messages
 // allowing the manager to make sure everyone is notified of a message
 type Conversation struct {
-	ID          string
+	ID          string `db:"id"`
 	Conversants []Conversant
 	Messages    []Message
-	Direct      bool
+	Direct      bool `db:"direct"`
 }
